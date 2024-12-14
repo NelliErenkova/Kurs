5190b5b098b6a86bf77a2cd05c881570150efbbb
NelliErenkova <erenkovanelli@gmail.com>
2024-12-15 02:28:47 +0300

    Fix2.0

--- app/src/main/java/com/example/fullbuddy/MainActivity.kt
+++ app/src/main/java/com/example/fullbuddy/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.fullbuddy
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.fullbuddy.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Настройка NavController вручную
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
     }
 }
